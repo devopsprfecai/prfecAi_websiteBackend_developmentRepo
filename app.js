aabc9065ff0eb790656850a7a3b80a45faf6e18e
thivagarradhakrishnan <ajithivagar@gmail.com>
2024-11-12 16:16:12 +0530

    hi

--- app.js
+++ app.js
@@ -12,16 +12,39 @@ const paymentController=require('./controllers/paymentController');
 const admin = require('./firebaseAdmin');
 const PORT = 6000;
 
-// Use CORS to allow requests from your frontend
 const corsOptions = {
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://trafy.ai', 'https://blog.trafy.ai'],
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'https://trafy.ai',
+        'https://blog.trafy.ai'
+    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'X-Requested-With',
+        'Accept',
+        'Origin',
+        'Access-Control-Allow-Origin',
+        'Access-Control-Allow-Headers',
+        'Access-Control-Allow-Methods'
+    ],
+    exposedHeaders: ['Set-Cookie'],
     credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204
 };
 
+
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('*', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', req.header('origin') || '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.status(204).send();
+});
 
 app.use(cookieParser());  // Parse cookies
 app.use(express.json());  // To parse JSON bodies
