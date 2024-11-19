8c83ec21165a73e928499eb997cdf3cbd5dd0b45
thivagarradhakrishnan <ajithivagar@gmail.com>
2024-11-19 11:07:54 +0530

    hii

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const paymentController=require('./controllers/paymentController');
 
 // Firebase Admin
 const admin = require('./firebaseAdmin');
-const PORT = 6000;
+const PORT = 5000;
 
 
 
 const corsOptions = {
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://trafy.ai', 'https://blog.trafy.ai'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://trafy-websiteclone-865611889264.us-central1.run.app', 'https://trafy-blogclone-865611889264.us-central1.run.app'],
     methods: ['GET', 'POST', 'OPTIONS'], // Include OPTIONS for preflight
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'], // Include necessary headers
     credentials: true, // Important for cookies
