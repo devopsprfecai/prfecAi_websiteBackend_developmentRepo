55b1c8f4e3d472b88e8eeb58055d61d3810449f4
thivagarradhakrishnan <ajithivagar@gmail.com>
2025-01-17 17:41:42 +0530

    hi

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -1,41 +1,20 @@
 const admin = require('firebase-admin');
-const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+// const serviceAccount = require('../config/serviceAccount.json');
 
-// Initialize the Secret Manager client
-const secretClient = new SecretManagerServiceClient();
-
-// Function to access a secret
-async function accessSecret(name) {
-    const [version] = await secretClient.accessSecretVersion({ name });
-    return version.payload.data.toString();
+// Ensure Firebase Admin SDK is initialized only once
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert({
+            // serviceAccount,
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        }),
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+    });
 }
 
-// Firebase initialization promise
-const firebaseInitPromise = (async () => {
-    if (!admin.apps.length) {
-        const FIREBASE_PROJECT_ID = await accessSecret('projects/592134571427/secrets/FIREBASE_PROJECT_ID/versions/1');
-        const FIREBASE_CLIENT_EMAIL = await accessSecret('projects/592134571427/secrets/FIREBASE_CLIENT_EMAIL/versions/1');
-        const FIREBASE_PRIVATE_KEY = await accessSecret('projects/592134571427/secrets/FIREBASE_PRIVATE_KEY/versions/1');
+const auth = admin.auth();
+const firestore = admin.firestore();
 
-        admin.initializeApp({
-            credential: admin.credential.cert({
-                projectId: FIREBASE_PROJECT_ID,
-                clientEmail: FIREBASE_CLIENT_EMAIL,
-                privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-            }),
-            databaseURL: `https://${FIREBASE_PROJECT_ID}.firebaseio.com`,
-        });
-    }
-})();
-
-// Export a wrapper for Firebase services to ensure initialization
-const getFirebaseServices = async () => {
-    await firebaseInitPromise;
-    return {
-        admin,
-        auth: admin.auth(),
-        firestore: admin.firestore(),
-    };
-};
-
-module.exports = getFirebaseServices;
+module.exports = { admin, auth, firestore };
