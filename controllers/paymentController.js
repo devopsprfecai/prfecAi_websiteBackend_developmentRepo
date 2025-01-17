aab7029be192087c720f3abbd6e76d8e8772eefb
thivagarradhakrishnan <ajithivagar@gmail.com>
2025-01-17 16:16:33 +0530

    hi

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -104,7 +104,6 @@ const sendPaymentEmail = async (req, res) => {
     }
 };
 
-
 const sendSignInEmail = async (req, res) => {
     const { email } = req.body;
 
@@ -120,34 +119,89 @@ const sendSignInEmail = async (req, res) => {
         };
         const link = await admin.auth().generateSignInWithEmailLink(email, actionCodeSettings);
 
+        // URL of the logo
+        const logoUrl = 'https://firebasestorage.googleapis.com/v0/b/prfecai-auth.firebasestorage.app/o/Prfec%20Logo%20White.png?alt=media&token=99410079-cdfe-4d04-bc83-c488b3d26916';
+
         // Send custom sign-in email with Nodemailer
         const mailOptions = {
             from: '"Your Project Team" <in.trafyai@gmail.com>',
             to: email,
             subject: 'Sign In to Your Account',
             html: `
-                <div style="text-align: center; font-family: Arial, sans-serif;">
-                    <h2>Sign In to Your Account</h2>
-                    <p>Click the button below to sign in:</p>
-                    <a href="${link}" style="text-decoration: none;">
-                        <button style="
-                            background-color: #4CAF50;
-                            border: none;
-                            color: white;
-                            padding: 12px 24px;
+            <html>
+                <head>
+                    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">                    
+                    <style>
+                        body {
+                            background-color: #f9f9f9;
+                            margin: 0;
+                            padding: 0;
+                        }
+                        .email-container {
+                            width: 100%;
+                            max-width: 600px;
+                            margin: 0 auto;
+                            padding: 20px;
+                            background-color: #000000; /* Black background */
+                            color: #ffffff; /* White text */
+                            border-radius: 8px;
                             text-align: center;
+                        }
+                        .button {
+                            background-color: #ffffff; /* White button background */
+                            color: #000000; /* Black text on the button */
+                            border: none;
+                            border-radius: 8px;
+                            padding: 12px 24px;
                             text-decoration: none;
+                            font-size: 15px;
+                            margin: 0;
                             display: inline-block;
-                            font-size: 16px;
-                            margin: 4px 2px;
+                            font-weight: 500;
                             cursor: pointer;
-                            border-radius: 8px;">
-                            Sign In
-                        </button>
-                    </a>
-                    <p>If you did not request this email, you can safely ignore it.</p>
-                </div>
-            `,
+                            font-family:"Inter",serif;
+                        }
+                        .email-container img {
+                            margin: 20px auto; /* Center image */
+                            max-width: 120px; /* Limit image size */
+                            display: block; /* Ensure it's block-level for centering */
+                            border: none; /* Remove default border */
+                        }
+                        h1{
+                            margin-top:40px;
+                            font-size:24px;
+                            font-weight:600;
+                            color:white;
+                            font-family:"Inter",serif;
+
+                            }
+                        p {
+                            margin-top:10px;
+                            margin-bottom:40px;
+                            font-size: 14px;
+                            color:#d1d1d1;
+                            font-family:"Inter",serif;
+
+                        }
+                    </style>
+                </head>
+                <body>
+                    <table width="100%" cellpadding="0" cellspacing="0" border="0" style="background-color: #f9f9f9; padding: 20px;">
+                        <tr>
+                            <td align="center">
+                                <div class="email-container">
+                                    <img src="${logoUrl}" alt="Logo">
+                                    <h1>Let's get you signed in</h1>
+                                    <p>All you have to do is click this button and we'll sign you in with a secure link</p>
+                                    <a href="${link}" class="button" style="color:black;">Sign in to prfec</a>
+                                    <p style="font-size:13px;color:#d1d1d1;font-weight:400;margin: 40px 0;">If you did not request this email, you can safely ignore it.</p>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </body>
+            </html>
+        `,
         };
 
         await transporter.sendMail(mailOptions);
@@ -157,7 +211,6 @@ const sendSignInEmail = async (req, res) => {
         res.status(500).json({ success: false, msg: 'Failed to send sign-in email' });
     }
 };
-
 
 
 module.exports = {
--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -1,20 +1,35 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('../config/serviceAccount.json');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
 // Ensure Firebase Admin SDK is initialized only once
-if (!admin.apps.length) {
-    admin.initializeApp({
-        credential: admin.credential.cert({
-            serviceAccount,
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-        }),
-        databaseURL: process.env.FIREBASE_DATABASE_URL,
-    });
+const secretClient = new SecretManagerServiceClient();
+
+async function accessSecret(name) {
+    const [version] = await secretClient.accessSecretVersion({ name });
+    return version.payload.data.toString();
 }
+
+(async () => {
+    if (!admin.apps.length) {
+        const FIREBASE_PROJECT_ID = await accessSecret('projects/592134571427/secrets/FIREBASE_PROJECT_ID/versions/1');
+        const FIREBASE_CLIENT_EMAIL = await accessSecret('projects/592134571427/secrets/FIREBASE_CLIENT_EMAIL/versions/1');
+        const FIREBASE_PRIVATE_KEY = await accessSecret('projects/592134571427/secrets/FIREBASE_PRIVATE_KEY/version/1');
+
+        admin.initializeApp({
+            credential: admin.credential.cert({
+                projectId: FIREBASE_PROJECT_ID,
+                clientEmail: FIREBASE_CLIENT_EMAIL,
+                privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+            }),
+            databaseURL: `https://${FIREBASE_PROJECT_ID}.firebaseio.com`,
+        });
+    }
+})();
 
 const auth = admin.auth();
 const firestore = admin.firestore();
+
+module.exports = { admin, auth, firestore };
 
 module.exports = { admin, auth, firestore };
