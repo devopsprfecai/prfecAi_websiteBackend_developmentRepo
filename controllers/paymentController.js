7ef766f2b5fcc61ccea307cf29418b42ff1db65f
thivagarradhakrishnan <ajithivagar@gmail.com>
2025-02-25 12:00:34 +0530

    hi

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -18,8 +18,8 @@ const transporter = nodemailer.createTransport({
     port: 465, // Common port for SMTP with STARTTLS
     secure: true, // Set to true for port 465 (SSL), or false for port 587 (STARTTLS)
     auth: {
-        user: 'devopsprfecai@gmail.com', // Your custom email address
-        pass: 'lgeg tbcr etyg srdw' // Your email password
+        user: 'prfecaimc@gmail.com', // Your custom email address
+        pass: 'cluo uyjt ucql nhod' // Your email password
     }
 });
 // console.log('Razorpay Key:', process.env.RAZORPAY_ID_KEY);
@@ -32,7 +32,7 @@ const transporter = nodemailer.createTransport({
 const sendEmailNotification = async (email, subject, message) => {
     try {
         await transporter.sendMail({
-            from: "in.trafyai@gmail.com",
+            from: '"Your Project Team" <prfecaimc@gmail.com>',
             to: email,
             subject: subject,
             text: message
@@ -70,7 +70,7 @@ const sendSignInEmail = async (req, res) => {
             html: `
             <html>
                 <head>
-                    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">                    
+                    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">                   
                     <style>
                         body {
                             background-color: #f9f9f9;
